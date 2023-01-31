5b7a21c6d05a2e4eaa66590a94e54701d235cd4d
user <user@DESKTOP-D1T4L74>
2023-01-31 14:32:20 +0530

    commit

---
+++ identifiers/src/identifiers/demo.java
@@ -0,0 +1,23 @@
+package identifiers;
+
+import java.util.*;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+				int x = 13;
+				System.out.println("value of the number variable is: "+x);
+				int number2 = 13; 
+				System.out.println("value of the number variable is: "+ number2);
+				
+				int Snumber=20; //starts with S
+				System.out.println("value of the number variable is: "+ Snumber);
+				String studentName="Vaishnavi"; 
+				System.out.println("value is "+ studentName);
+				
+
+	}
+
+}
